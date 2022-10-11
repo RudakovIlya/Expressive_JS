7953cf479077e14464dbce1177e0b27ec48e56ab
Rudakov Ilya <rudakov.alhan@yandex.ru>
2022-10-11 16:03:25 +0300

    feat: added solution task The sum of a range

--- index.js
+++ index.js
@@ -58,10 +58,38 @@ const countChar = (str, char) => {
 }
 
 const countBs = (str) => {
-	return countChar(string, "B");
+	return countChar(str, "B");
 }
 
 console.log(countBs("BBC"));
 // → 2
 console.log(countChar("kakkerlak", "k"));
 // → 4
+
+/*===================================================================================*/
+
+// Chapter: Data structures (objects and arrays)
+
+// Task: The sum of a range
+
+// Solution: 
+
+const range = (start, end, step = start < end ? 1 : -1) => {
+	const arr = [];
+
+	if (step > 0) {
+		for (let i = start; i <= end; i += step) arr.push(i);
+	} else {
+		for (let i = start; i >= end; i += step) arr.push(i);
+	}
+	return arr;
+}
+
+const sum = (arr) => {
+	let sum = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		sum += arr[i]
+	}
+	return sum
+}
